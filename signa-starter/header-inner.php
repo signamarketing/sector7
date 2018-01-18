67daf0f4129a3aa5b0f5cfe670c19b4250d82f27
DonVentura <don@signamarketing.com>
2018-01-18 10:53:02 -0700

    Google Tag Manager added

--- signa-starter/header-inner.php
+++ signa-starter/header-inner.php
@@ -14,35 +14,27 @@
 	<html <?php language_attributes(); ?>>
 
 	<head>
+		<!-- Google Tag Manager -->
+		<script>
+		(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+		new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+		j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+		'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+		})(window,document,'script','dataLayer','GTM-KQ2747V');
+		</script>
+		<!-- End Google Tag Manager -->
 		<meta charset="<?php bloginfo( 'charset' ); ?>">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<link rel="profile" href="http://gmpg.org/xfn/11">
 		<link href="https://fonts.googleapis.com/css?family=Heebo:300,400,700,900" rel="stylesheet">
 		<script src="/wp-content/themes/signa-starter/js/master-js.js"></script>
 		<?php wp_head(); ?>
-		<!-- Google Tag Manager -->
-		<script>
-			(function (w, d, s, l, i) {
-				w[l] = w[l] || [];
-				w[l].push({‘
-					gtm.start’: new Date().getTime(),
-					event: ‘gtm.js’
-				});
-				var f = d.getElementsByTagName(s)[0],
-					j = d.createElement(s),
-					dl = l != ‘dataLayer’ ? ‘ & l = ‘+l : ‘’;
-				j.async = true;
-				j.src = ’https: //www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-			})(window, document, ‘script’, ‘dataLayer’, ‘GTM - KQ2747V’);
-		</script>
-		<!-- End Google Tag Manager -->
 	</head>
 
 	<body <?php body_class(); ?>>
-		<noscript>
-			<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KQ2747V”
-height=“0" width=“0” style=“display:none;visibility:hidden”></iframe>
-		</noscript>
+		<!-- Google Tag Manager (noscript) -->
+			<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KQ2747V"
+			height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 		<!-- End Google Tag Manager (noscript) -->
 		<div id="page" class="site">
 
