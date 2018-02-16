a3b8ab8163a82f3d9c6968c3153d9f7e79a8b848
DonVentura <don@signamarketing.com>
2018-02-16 11:09:29 -0700

    updating live

--- signa-starter/header.php
+++ signa-starter/header.php
@@ -26,7 +26,7 @@
 		<meta charset="<?php bloginfo( 'charset' ); ?>">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<link rel="profile" href="http://gmpg.org/xfn/11">
-		<script type="text/javascript" src="wp-content/themes/signa-starter/js/retina.min.js"></script>
+
 		<?php wp_head(); ?>
 
 	</head>
