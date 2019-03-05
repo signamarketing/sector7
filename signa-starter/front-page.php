392298f5cf084f9eecd71693bc668f65ed73b042
Trevor Franklin <tdfranklinaz@gmail.com>
2019-03-05 10:38:49 -0700

    fixed noscript error

--- signa-starter/front-page.php
+++ signa-starter/front-page.php
@@ -94,7 +94,7 @@ get_header(); ?>
 		<div class="container-fluid main-nf-bg" style="position: relative; display: block; background: radial-gradient(#4cced1 19% ,#34bec1);">
 			<div class="row row-container">
 				<div class="col-12">
-					<p class="bold-statement text-center" style="padding-bottom: 25px;>
+					<p class="bold-statement text-center" style="padding-bottom: 25px;">
 						Need an App? <br>
 						Let's Chat
 					</p>
