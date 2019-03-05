712010665d584e8535532f94270e3c6323b88573
Trevor Franklin <tdfranklinaz@gmail.com>
2019-03-05 12:55:01 -0700

    more spacing styles mobile

--- signa-starter/front-page.php
+++ signa-starter/front-page.php
@@ -60,7 +60,7 @@ get_header(); ?>
 			</div>
 		</div>
 		<div class="container-fluid no-gutter bold-statement-bg">
-			<div class="row row-container" style="padding: 0px; min-height: 354px;">
+			<div class="row row-container" style="padding: 60px 0px 0px 0px; min-height: 354px;">
 				<div class="col-md-6 fo2 text-center"><img class="bold-statement-img" src="/wp-content/uploads/2018/03/bold-statement-banner-img-2.png"></div>
 				<div class="col-md-6 fo1 f-center" style="align-items: center; display: flex;">
 					<p class="bold-statement">Make a Bold Statement</p>
