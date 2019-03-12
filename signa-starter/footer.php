b4aca047bd3f6d80c0f570a20e765a9078c7749f
Trevor Franklin <tdfranklinaz@gmail.com>
2019-03-12 16:32:05 -0700

    added lazy loading

--- signa-starter/footer.php
+++ signa-starter/footer.php
@@ -11,41 +11,42 @@
 
 ?>
 
-	</div>
-	<!-- #content -->
-	<footer>
-	<div id="colophon" class="site-footer container-fluid no-gutter">
-		<div id="main-footer-section" class="row row-container">
-		<div id="footer-sidebar3" class="col-md-6 f-center" style="align-items: center; display: flex; margin-right:auto;">
-				<?php if(is_active_sidebar('footer-sidebar-3')){ dynamic_sidebar('footer-sidebar-3');}?>
-			</div>
-			<div id="footer-sidebar1" class="col-md-3 col-lg-3 f-center">
-				<?php if(is_active_sidebar('footer-sidebar-1')){ dynamic_sidebar('footer-sidebar-1');}?>
+		</div><!-- #content -->
+		<footer>
+
+			<div id="colophon" class="site-footer container-fluid no-gutter">
+				<div id="main-footer-section" class="row row-container">
+					<div id="footer-sidebar3" class="col-md-6 f-center" style="align-items: center; display: flex; margin-right:auto;">
+						<?php if(is_active_sidebar('footer-sidebar-3')){ dynamic_sidebar('footer-sidebar-3');}?>
+					</div>
+					<div id="footer-sidebar1" class="col-md-3 col-lg-3 f-center">
+						<?php if(is_active_sidebar('footer-sidebar-1')){ dynamic_sidebar('footer-sidebar-1');}?>
+					</div>
+
+					<div id="footer-sidebar2" class="col-md-3 col-lg-3 f-center">
+						<?php if(is_active_sidebar('footer-sidebar-2')){ dynamic_sidebar('footer-sidebar-2');}?>
+					</div>
+				</div><!-- .site-info -->
 			</div>
 
-			<div id="footer-sidebar2" class="col-md-3 col-lg-3 f-center">
-				<?php if(is_active_sidebar('footer-sidebar-2')){ dynamic_sidebar('footer-sidebar-2');}?>
+			<div class="copyright-section">
+				<div class="row row-container text-center" style="padding:0px;">
+					<p class="copyright-text">
+						<a href="#">Sitemap</a> | © Copyright 2019, Sector 7 Apps, All Rights reserved
+					</p>
+				</div>
 			</div>
 
+		</footer><!-- #colophon -->
+		</div><!-- #page -->
 
-		</div>
-		<!-- .site-info -->
-</div>
-	<div class="copyright-section">
-			<div class="row row-container text-center" style="padding:0px;">
-				<p class="copyright-text">
-					<a href="#">Sitemap</a> | © Copyright 2019, Sector 7 Apps, All Rights reserved
-				</p>
-			</div>
-		</div>
-</footer>
-	<!-- #colophon -->
-	</div>
-	<!-- #page -->
-	<script src="https://use.fontawesome.com/52dda3483b.js"></script>	
-	<script src="/wp-content/themes/signa-starter/js/master-js.js"></script>
-	<?php wp_footer(); ?>
+		<script src="https://use.fontawesome.com/52dda3483b.js"></script>	
+		<script src="/wp-content/themes/signa-starter/js/master-js.js"></script>
+
+		<!-- Lazy Loading -->
+		<script src="https://cdn.jsdelivr.net/npm/vanilla-lazyload@11.0.5/dist/lazyload.min.js"></script>
+
+		<?php wp_footer(); ?>
 
 	</body>
-
-	</html>
+</html>
--- signa-starter/js/master-js.js
+++ signa-starter/js/master-js.js
@@ -1,27 +1,32 @@
-/*MENU SCROLL FUNCTION*/
-window.addEventListener('scroll', function () {
-    document.body.classList[
-      document.documentElement.scrollTop > 125 ? 'add': 'remove'
-    ]('scrolled');
-  });
-
-/*MENU TOGGLE*/
-  var classOne    = document.querySelector("#primary-menu");
-  var classTwo    = document.querySelector("#open-menu-btn");
-  var classThree  = document.querySelector("#close-menu-btn");
-  var classFour  = document.querySelector("#navbar");
-  var classFive  = document.querySelector(".navbar-container");
-  
-  var allOpenMenuClasses = [classOne, classTwo, classThree, classFour, classFive];
-
-
-  function openMenu() {
-    allOpenMenuClasses.forEach (function(el) {
-        el.classList.add("open")
-    })
-  }
-  function closeMenu() {
-    allOpenMenuClasses.forEach(function(el) {
-        el.classList.remove('open')
-    })
-  }
+/*MENU SCROLL FUNCTION*/
+window.addEventListener('scroll', function () {
+  document.body.classList[
+    document.documentElement.scrollTop > 125 ? 'add' : 'remove'
+  ]('scrolled');
+});
+
+/*MENU TOGGLE*/
+var classOne = document.querySelector("#primary-menu");
+var classTwo = document.querySelector("#open-menu-btn");
+var classThree = document.querySelector("#close-menu-btn");
+var classFour = document.querySelector("#navbar");
+var classFive = document.querySelector(".navbar-container");
+
+var allOpenMenuClasses = [classOne, classTwo, classThree, classFour, classFive];
+
+function openMenu() {
+  allOpenMenuClasses.forEach(function (el) {
+    el.classList.add("open")
+  })
+}
+
+function closeMenu() {
+  allOpenMenuClasses.forEach(function (el) {
+    el.classList.remove('open')
+  })
+}
+
+/*LAZY LOADING*/
+var lazyLoadInstance = new LazyLoad({
+  elements_selector: ".lazy"
+});
