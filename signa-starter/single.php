7c0fdd78f6d185c9185e25aee42484d3a9cb5bcb
Trevor Franklin <tdfranklinaz@gmail.com>
2019-03-20 17:57:48 -0700

    added container to blog single

--- signa-starter/single.php
+++ signa-starter/single.php
@@ -12,20 +12,28 @@ get_header('inner'); ?>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main">
 
-		<?php
-		while ( have_posts() ) : the_post();
+		<div class="container-fluid no-gutters">
+			<div class="row row-container">
 
-			get_template_part( 'template-parts/content', get_post_type() );
+			<?php
+			while ( have_posts() ) : the_post();
 
-			the_post_navigation();
+				get_template_part( 'template-parts/content', get_post_type() );
 
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
+				the_post_navigation();
 
-		endwhile; // End of the loop.
-		?>
+				// If comments are open or we have at least one comment, load up the comment template.
+				if ( comments_open() || get_comments_number() ) :
+					comments_template();
+				endif;
+
+			endwhile; // End of the loop.
+			?>
+
+			</div>
+		</div>
+
+		
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
