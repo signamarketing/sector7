7cb001eeef5e17e24e6016a2aca024b65dde7c16
Trevor Franklin <tdfranklinaz@gmail.com>
2019-03-21 10:46:42 -0700

    after content meta

--- signa-starter/single.php
+++ signa-starter/single.php
@@ -27,9 +27,54 @@ get_header('inner'); ?>
 
 						<img src="<?php echo $thumb_url; ?>" alt="" class="img-fit">
 
-						<?php the_content();
+						<?php the_content(); ?>
 
-						the_post_navigation(); ?>
+						<div class="after-post-meta">
+							<br>
+							<div class="end-of-post-categories">
+								<div class="end-of-post-categories-text">Previous & Next Posts</div>
+								<div class="end-of-post-categories-separator"></div>
+							</div>
+							<div class="single-nav-column post-33-container">
+							<?php $prevPost = get_previous_post(false);
+								if ($prevPost) {?>
+								<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+									<header class="entry-header">
+										<div class="post-img-box">
+											<?php $prevthumbnail = get_the_post_thumbnail($prevPost->ID);?>
+											<?php previous_post_link('%link',"$prevthumbnail", false);  ?>
+										</div>
+										<div id="blog-date-text">
+											<?php $prev_post = get_previous_post(); ?><?php echo mysql2date('F, j Y', $prev_post->post_date, false)?> 
+										</div>
+										<div class="single-nav-title-text">
+											<?php previous_post_link('%link',"<h3 class=\"post-title\">%title</h3>", false);  ?>
+										</div>
+									</header>
+								</article><?php } ?>
+							</div>
+
+							<div class="single-nav-column post-33-container">
+							<?php $nextPost = get_next_post(false);
+								if ($nextPost) {?>
+								<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+									<header class="entry-header">
+										<div class="post-img-box">
+											<?php $nextthumbnail = get_the_post_thumbnail($nextPost->ID);?>
+											<?php next_post_link('%link',"$nextthumbnail", false);  ?>
+										</div>
+										<div id="blog-date-text">
+											<?php $next_post = get_next_post(); ?><?php echo mysql2date('F, j Y', $next_post->post_date, false)?> 
+										</div>
+										<div class="single-nav-title-text">
+											<?php next_post_link('%link',"<h3 class=\"post-title\">%title</h3>", false);  ?>
+										</div>
+										<?php } ?>
+										
+									</header>
+								</article>
+							</div>
+						</div>
 
 					<?php endwhile; // End of the loop. ?>
 
