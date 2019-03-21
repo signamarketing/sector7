892b2097e9f9a69788843549019fd382d8b52d89
Trevor Franklin <tdfranklinaz@gmail.com>
2019-03-21 11:18:28 -0700

    after post section

--- signa-starter/single.php
+++ signa-starter/single.php
@@ -29,57 +29,6 @@ get_header('inner'); ?>
 
 						<?php the_content(); ?>
 
-						<div class="after-post-meta">
-							<br>
-							<div class="end-of-post-categories">
-								<div class="end-of-post-categories-text">Previous & Next Posts</div>
-								<div class="end-of-post-categories-separator"></div>
-							</div>
-							<div class="row">
-
-								<div class="single-nav-column post-33-container col">
-								<?php $prevPost = get_previous_post(false);
-									if ($prevPost) {?>
-									<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-										<header class="entry-header">
-											<div class="post-img-box">
-												<?php $prevthumbnail = get_the_post_thumbnail($prevPost->ID);?>
-												<?php previous_post_link('%link',"$prevthumbnail", false);  ?>
-											</div>
-											<div id="blog-date-text">
-												<?php $prev_post = get_previous_post(); ?><?php echo mysql2date('F, j Y', $prev_post->post_date, false)?> 
-											</div>
-											<div class="single-nav-title-text">
-												<?php previous_post_link('%link',"<h3 class=\"post-title\">%title</h3>", false);  ?>
-											</div>
-										</header>
-									</article><?php } ?>
-								</div>
-
-								<div class="single-nav-column post-33-container col">
-								<?php $nextPost = get_next_post(false);
-									if ($nextPost) {?>
-									<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-										<header class="entry-header">
-											<div class="post-img-box">
-												<?php $nextthumbnail = get_the_post_thumbnail($nextPost->ID);?>
-												<?php next_post_link('%link',"$nextthumbnail", false);  ?>
-											</div>
-											<div id="blog-date-text">
-												<?php $next_post = get_next_post(); ?><?php echo mysql2date('F, j Y', $next_post->post_date, false)?> 
-											</div>
-											<div class="single-nav-title-text">
-												<?php next_post_link('%link',"<h3 class=\"post-title\">%title</h3>", false);  ?>
-											</div>
-											<?php } ?>
-											
-										</header>
-									</article>
-								</div>
-								
-							</div>
-						</div>
-
 					<?php endwhile; // End of the loop. ?>
 
 				</div>
@@ -88,6 +37,64 @@ get_header('inner'); ?>
 
 			</div>
 		</div>
+
+		<!-- After post content -->
+		<section class="bg-ltgrey">
+			<div class="container-fluid no-gutters">
+				<div class="row row-container">
+					<div class="after-post-meta">
+						<br>
+						<div class="end-of-post-categories">
+							<div class="end-of-post-categories-text">Previous & Next Posts</div>
+							<div class="end-of-post-categories-separator"></div>
+						</div>
+						<div class="row">
+
+							<div class="single-nav-column post-33-container col">
+							<?php $prevPost = get_previous_post(false);
+								if ($prevPost) {?>
+								<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+									<header class="entry-header">
+										<div class="post-img-box">
+											<?php $prevthumbnail = get_the_post_thumbnail($prevPost->ID);?>
+											<?php previous_post_link('%link',"$prevthumbnail", false);  ?>
+										</div>
+										<div id="blog-date-text">
+											<?php $prev_post = get_previous_post(); ?><?php echo mysql2date('F, j Y', $prev_post->post_date, false)?> 
+										</div>
+										<div class="single-nav-title-text">
+											<?php previous_post_link('%link',"<h3 class=\"post-title\">%title</h3>", false);  ?>
+										</div>
+									</header>
+								</article><?php } ?>
+							</div>
+
+							<div class="single-nav-column post-33-container col">
+							<?php $nextPost = get_next_post(false);
+								if ($nextPost) {?>
+								<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+									<header class="entry-header">
+										<div class="post-img-box">
+											<?php $nextthumbnail = get_the_post_thumbnail($nextPost->ID);?>
+											<?php next_post_link('%link',"$nextthumbnail", false);  ?>
+										</div>
+										<div id="blog-date-text">
+											<?php $next_post = get_next_post(); ?><?php echo mysql2date('F, j Y', $next_post->post_date, false)?> 
+										</div>
+										<div class="single-nav-title-text">
+											<?php next_post_link('%link',"<h3 class=\"post-title\">%title</h3>", false);  ?>
+										</div>
+										<?php } ?>
+										
+									</header>
+								</article>
+							</div>
+							
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
 
 		
 
