c6e2f51906b2359bf9771dc0b296034f5fd8275b
Trevor Franklin <tdfranklinaz@gmail.com>
2019-03-22 16:25:36 -0700

    added cta back to page

--- signa-starter/category.php
+++ signa-starter/category.php
@@ -107,6 +107,16 @@ get_header('inner'); ?>
           </div>
         </div>
 
+        <!-- Form CTA -->
+        <div class="container-fluid main-nf-bg" style="position: relative; display: block; background: radial-gradient(#4cced1 19%,#34bec1);">
+          <div class="row row-container">
+            <div class="col-12">
+              <p class="bold-statement text-center" style="padding-bottom: 25px;">Need an App?<br>Let's Chat</p>
+              <?php echo do_shortcode('[ninja_form id=2]'); ?>
+            </div>
+          </div>
+        </div>
+
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
