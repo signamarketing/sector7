6fbae9f82cd76bf0cedebdb4b3b3bb1339f89242
Don <donventura29@gmail.com>
2019-04-24 16:01:45 -0700

    Error causing php removed until ready to troubleshoot

--- signa-starter/single.php
+++ signa-starter/single.php
@@ -15,7 +15,9 @@ get_header('single'); ?>
     <div class="blog-container">
 
       <?php 
-
+        // if(is_singular('post')) {
+        //   get_template_part('template-parts/content', 'socialshare');
+        // }
       ?>
 
       <div class="blog-row">
@@ -28,57 +30,34 @@ get_header('single'); ?>
             <div class="entry-meta blog-entry-meta">
           
               <div class="blog-photo-meta">
-                <?php 
-                  function my_display_gravatar() { 
-                    // Get User Email Address
-                    $getuseremail = get_the_author_meta('user_email');
-              
-                    // Convert email into md5 hash and set image size to 200 px
-                    $usergravatar = 'https://www.gravatar.com/avatar/' . md5($getuseremail) . '?s=90';
-                    echo '<img src="' . $usergravatar . '" class="wpb_gravatar blog-gravatar" />';
-                  } 
-                  echo my_display_gravatar();
-                ?>
+
               </div>
 
               <div class="blog-text-meta">
 
                 <div class="blog-author-name"><?php echo the_author_meta('display_name'); ?></div>
                 <div class="blog-post-date">
-                  <?php 
-                    $post_date = get_the_date( 'F j, Y' ); 
-                    $lastupdated = get_field('display-last-updated');
-
-                    if($lastupdated) {
-
-                      // show last updated date
-                      $u_time = get_the_time('U'); 
-                      $u_modified_time = get_the_modified_time('U');
-                      if ($u_modified_time >= $u_time + 86400) {
-                        the_modified_time('F jS, Y'); 
-                      }
-
-                    } else {
-                      echo $post_date;
-                    }
-                  ?>
+                 
                 </div>
 
                 <div>
                   <div class="reading-time"></div>
-                </div>
-                  
+								</div>
+								
               </div>
-
-              
-
-
             </div><!-- .entry-meta -->
           <?php endif; ?>
 
         </div>
         <div class="blog-half">
-          <!-- <img src="<?php the_post_thumbnail_url() ?>" alt="<?php the_title(); ?>" class="img-fluid" loading="lazy"> -->
+				<?php
+						$thumb_id = get_post_thumbnail_id();
+						$thumb_url_array = wp_get_attachment_image_src($thumb_id, 'thumbnail-size', true);
+						$thumb_url = $thumb_url_array[0];
+					?>
+
+						<img src="<?php echo $thumb_url; ?>" alt="" class="img-fit">
+          <img src="<?php $thumb_url ?>" alt="<?php the_title(); ?>" class="img-fluid" loading="lazy">
         </div>
       </div>
     </div>
@@ -110,7 +89,7 @@ get_header('single'); ?>
 
 				<?php while ( have_posts() ) : the_post(); ?>
 
-						<?php the_content(); ?>
+						<?php the_content() ?>
 
 					<?php endwhile; // End of the loop. ?>
 
