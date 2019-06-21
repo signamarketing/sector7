9d0729f17d925b4dfc5cc2f0a6310dfacdc3feb8
Don <donventura29@gmail.com>
2019-06-20 18:20:22 -0700

    adding class to image

--- front-page.php
+++ front-page.php
@@ -81,9 +81,9 @@ get_header(); ?>
 			<div class="row row-container bold-statement-box">
 				<div class="col-md-6 col-lg-5 fo2 text-center">
 					<picture>
-						<source data-srcset="/wp-content/themes/signa-starter/img/webp/bold-statement-banner-img-2.webp" type="image/webp" class="lazy" alt="three shiny phones" >
-						<source data-srcset="/wp-content/uploads/2018/03/bold-statement-banner-img-2.png" type="image/png" class="lazy" alt="three shiny phones" > 
-						<img data-src="/wp-content/uploads/2018/03/bold-statement-banner-img-2.png" class="lazy" alt="three shiny phones" >
+						<source data-srcset="/wp-content/themes/signa-starter/img/webp/bold-statement-banner-img-2.webp" type="image/webp" class="bold-statment-img lazy" alt="three shiny phones" >
+						<source data-srcset="/wp-content/uploads/2018/03/bold-statement-banner-img-2.png" type="image/png" class="bold-statement-img lazy" alt="three shiny phones" > 
+						<img data-src="/wp-content/uploads/2018/03/bold-statement-banner-img-2.png" class="bold-statement-img lazy" alt="three shiny phones" >
           </picture>
 				</div>
 				<div class="col-md-6 col-lg-4 fo1 f-center" style="align-items: center; display: flex;">
