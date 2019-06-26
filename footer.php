47bc1addce044b1b26a58eee35547bcc31746b2b
Don <donventura29@gmail.com>
2019-06-26 11:49:31 -0700

    adding animation library cdn links to website

--- footer.php
+++ footer.php
@@ -64,7 +64,7 @@
 <?php wp_footer(); ?>
 
 <script src="/wp-content/themes/signa-starter/js/master-js.js"></script>
-
+<script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
 </body>
 
 </html>
