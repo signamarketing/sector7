82a84b8bfa1b00d55314621f8b1fc4cca2b7fb03
Trevor Franklin <tdfranklinaz@gmail.com>
2019-07-26 09:29:50 -0700

    added classes for contained 404 templates

--- 404.php
+++ 404.php
@@ -12,46 +12,58 @@ get_header('inner'); ?>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main">
 
-			<section class="error-404 not-found">
-				<header class="page-header">
-					<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'signa-starter' ); ?></h1>
-				</header><!-- .page-header -->
+			<div class="container-fluid no-gutters">
+				<div class="row row-container">
 
-				<div class="page-content">
-					<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'signa-starter' ); ?></p>
 
-					<?php
-						get_search_form();
+					<section class="error-404 not-found">
+						<header class="page-header">
+							<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'signa-starter' ); ?></h1>
+						</header><!-- .page-header -->
 
-						the_widget( 'WP_Widget_Recent_Posts' );
-					?>
+						<div class="page-content">
+							<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'signa-starter' ); ?></p>
 
-					<div class="widget widget_categories">
-						<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'signa-starter' ); ?></h2>
-						<ul>
-						<?php
-							wp_list_categories( array(
-								'orderby'    => 'count',
-								'order'      => 'DESC',
-								'show_count' => 1,
-								'title_li'   => '',
-								'number'     => 10,
-							) );
-						?>
-						</ul>
-					</div><!-- .widget -->
+							<?php
+								get_search_form();
 
-					<?php
+								the_widget( 'WP_Widget_Recent_Posts' );
+							?>
 
-						/* translators: %1$s: smiley */
-						$archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'signa-starter' ), convert_smilies( ':)' ) ) . '</p>';
-						the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$archive_content" );
+							<div class="widget widget_categories">
+								<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'signa-starter' ); ?></h2>
+								<ul>
+								<?php
+									wp_list_categories( array(
+										'orderby'    => 'count',
+										'order'      => 'DESC',
+										'show_count' => 1,
+										'title_li'   => '',
+										'number'     => 10,
+									) );
+								?>
+								</ul>
+							</div><!-- .widget -->
 
-						the_widget( 'WP_Widget_Tag_Cloud' );
-					?>
+							<?php
 
-				</div><!-- .page-content -->
-			</section><!-- .error-404 -->
+								/* translators: %1$s: smiley */
+								$archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'signa-starter' ), convert_smilies( ':)' ) ) . '</p>';
+								the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$archive_content" );
+
+								the_widget( 'WP_Widget_Tag_Cloud' );
+							?>
+
+						</div><!-- .page-content -->
+					</section><!-- .error-404 -->
+				
+
+
+				</div>
+			</div>
+			
+
+			
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
