f26da420e16b349fd35113f2a973e480e1ee59b7
Trevor Franklin <tdfranklinaz@gmail.com>
2020-03-26 15:23:31 -0700

    test

--- footer.php
+++ footer.php
@@ -64,7 +64,7 @@
 <?php wp_footer(); ?>
 
 <script async src="/wp-content/themes/signa-starter/js/master-js.js"></script>
-<script async src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+<script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
 <script>
   AOS.init();
 </script>
