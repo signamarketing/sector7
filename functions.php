59b3823115ed6e72f7513e4246e8f6b5a1efbaf1
Trevor Franklin <tdfranklinaz@gmail.com>
2020-03-26 14:59:37 -0700

    added dashicons removal to live site

--- functions.php
+++ functions.php
@@ -223,3 +223,14 @@ function new_excerpt_more($more) {
 	return '<a class="nostyle" href="'. get_permalink($post->ID) . '">...</a>';
 }
 add_filter('excerpt_more', 'new_excerpt_more');
+
+/**
+ * Remove dashicons from front-end
+ */
+add_action( 'wp_print_styles',     'my_deregister_styles', 100 );
+
+function my_deregister_styles()    { 
+   //wp_deregister_style( 'amethyst-dashicons-style' ); 
+   wp_deregister_style( 'dashicons' ); 
+
+}
