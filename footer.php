00dc62b59f4ca40af7b5355f1cae87380dcdabd6
Trevor Franklin <tdfranklinaz@gmail.com>
2020-03-27 12:20:24 -0700

    added optimization to footer

--- footer.php
+++ footer.php
@@ -43,7 +43,7 @@
 
 </footer>
 </div><!-- #page -->
-<link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,700" rel="stylesheet">
+
 <!-- Lazy Loading -->
 <script src="https://cdn.jsdelivr.net/npm/vanilla-lazyload@11.0.5/dist/lazyload.min.js"></script>
 <script>
@@ -63,7 +63,7 @@
 
 <?php wp_footer(); ?>
 
-<script src="/wp-content/themes/signa-starter/js/master-js.js"></script>
+<script async src="/wp-content/themes/signa-starter/js/master-js.js"></script>
 <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
 <script>
   AOS.init();
