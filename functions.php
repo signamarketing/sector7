3f7d8a93574670b216578a2176df54d4c102ae56
Jacob J <jaureguijacob57@gmail.com>
2020-06-03 14:10:44 -0700

    readded script to remove dashicons on front end | remove block library
    block library is for gutenburg which isn't used at all. Still loads library on the front end

--- functions.php
+++ functions.php
@@ -227,16 +227,22 @@ add_filter('excerpt_more', 'new_excerpt_more');
 /**
  * Remove dashicons from front-end
  */
-// add_action( 'wp_print_styles',     'my_deregister_styles', 100 );
-
-// function my_deregister_styles()    { 
-//    //wp_deregister_style( 'amethyst-dashicons-style' ); 
-//    wp_deregister_style( 'dashicons' ); 
-
-// }
+function my_deregister_styles()    { 
+   //wp_deregister_style( 'amethyst-dashicons-style' ); 
+   wp_deregister_style( 'dashicons' ); 
+}
+add_action( 'wp_print_styles',     'my_deregister_styles', 100 );
 
 // To check if webp exists for a banner image.
 function check_404($url) {
 	$headers=get_headers($url, 1);
 	if ($headers[0]!='HTTP/1.1 200 OK') return true; else return false;
  }
+
+//Remove Gutenberg Block Library CSS from loading on the frontend
+function smartwp_remove_wp_block_library_css(){
+ wp_dequeue_style( 'wp-block-library' );
+ wp_dequeue_style( 'wp-block-library-theme' );
+ wp_dequeue_style( 'wc-block-style' ); // Remove WooCommerce block CSS
+}
+add_action( 'wp_enqueue_scripts', 'smartwp_remove_wp_block_library_css', 100 );
