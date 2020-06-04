5b1cb8204f238e8a2640830f4422a898dc4651ee
Jacob J <jaureguijacob57@gmail.com>
2020-06-04 14:04:46 -0700

    Added missing media queries | trying different gutenberg css removal

--- functions.php
+++ functions.php
@@ -248,7 +248,9 @@ function check_404($url) {
 	if ($headers[0]!='HTTP/1.1 200 OK') return true; else return false;
  }
 
- function wpassist_remove_block_library_css(){
-    wp_dequeue_style( 'wp-block-library' );
-} 
-add_action( 'wp_enqueue_scripts', 'wpassist_remove_block_library_css' );
+function smartwp_remove_wp_block_library_css(){
+	wp_dequeue_style( 'wp-block-library' );
+	wp_dequeue_style( 'wp-block-library-theme' );
+	wp_dequeue_style( 'wc-block-style' ); // Remove WooCommerce block CSS
+   }
+   add_action( 'wp_enqueue_scripts', 'smartwp_remove_wp_block_library_css', 100 );
